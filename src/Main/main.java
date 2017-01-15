a144df101fba6107a0ba90581e14051958f93a6b
KadriN <kadri.nigulas@gmail.com>
2017-01-15 15:51:16 +0200

    proov

---
+++ src/Main/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        // Prints "Alustame pommitamisega" to the terminal window.
+        System.out.println("Alustame pommitamisega");
+        System.out.println("Autor: Gena");
+        System.out.println("2017");
+    }
+
+}
